27ac34ad3c12da5857fdb82fe45cda12855afda8
vsmolenskiy <remedilessoptimist@gmail.com>
2017-01-26 17:22:06 +0200

    homework lesson3

---
+++ src/main/java/com/vsmolenskiy/app/homework/lesson3/units/convertors/Math.java
@@ -0,0 +1,47 @@
+package com.vsmolenskiy.app.homework.lesson3.units.convertors;
+
+/**
+ * Created by V.Smolenskyi on 26.01.2017.
+ */
+public class Math {
+    public int abc(int a){
+        return a;
+    }
+    public int cbrt (int a){
+        return a;
+    }
+    public int ceil (int a){
+        return a;
+    }
+    public int copySign (int magnitude, int sign){
+        return magnitude;
+    }
+    public double cos (double a){
+        return a;
+    }
+    public int decrementExact (int a){
+        return a-1;
+    }
+    public int incrementExact(int a){
+        return a+1;
+    }
+    public int max(int a, int b){
+        return (a > b) ? a : b;
+    }
+    public int min(int a, int b){
+        return (a > b) ? a : b;
+    }
+    public int negateExact(int a){
+        return a;
+    }
+    public int subtractExact(int x, int y){
+        return x - y;
+    }
+    public int division(int a, int b){
+        return a/b;
+    }
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+}
