61130eadc56667a824d9f59fb50b570401278856
vsmolenskiy <remedilessoptimist@gmail.com>
2017-01-30 22:11:52 +0200

    lesson5

--- src/test/java/com/vsmolenskiy/app/classwork/lesson5/ParametrizedTests.java
+++ src/test/java/com/vsmolenskiy/app/classwork/lesson5/ParametrizedTests.java
@@ -2,10 +2,9 @@ package com.vsmolenskiy.app.classwork.lesson5;
 
 import com.vsmolenskiy.app.classwork.lesson3.MathFunc;
 import junitparams.FileParameters;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import junitparams.JUnitParamsRunner;
+import junitparams.mappers.CsvWithHeaderMapper;
+import org.junit.*;
 import org.junit.runner.RunWith;
 
 /**
@@ -13,21 +12,14 @@ import org.junit.runner.RunWith;
  */
 @RunWith(JUnitParamsRunner.class)
 public class ParametrizedTests {
-    MathFunc mathFunc;
-    @BeforeClass
-    public  void setUpClass(){
+     MathFunc mathFunc;
+    @Before
+    public  void setUp(){
         mathFunc = new MathFunc();
     }
-
     @Test
-    @FileParameters(value = "src/test/resources/testdata.csv", mapper = CsvWithHeaderMapper.class)
+    @FileParameters(value = "src/test/resources/multiplyData.txt", mapper = CsvWithHeaderMapper.class)
     public void multiplyTest(int a, int b, int expected){
         Assert.assertEquals(expected, mathFunc.multiply(a,b));
     }
-
-    @AfterClass
-    public  void tearDownClass(){
-
-    }
-
 }
